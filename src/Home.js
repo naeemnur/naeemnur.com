79e938f62eaf4932d3c07b1c603e32a8aa9140bd
naeemMac <naeemnur@gmail.com>
2019-08-31 13:00:31 +0300

    added react-loading-skeleton

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,16 @@
 import React, { Component, Fragment } from "react";
 import { Helmet } from "react-helmet";
+import Skeleton from "react-loading-skeleton";
 
 class Home extends Component {
   render() {
     return (
       <Fragment>
         <Helmet>
-          <title>WordPress Developer Bahrain - Naeem Noor</title>
+          <title>WordPress Developer Bahrain - Naeem Noor </title>
           <meta name="description" content="Helmet application" />
         </Helmet>
-        <h1>Hello, there!</h1>
+        <h1>{"Hi, there!" || <Skeleton />}</h1>
         <p>
           I’m Naeem — I design, develop, deploy, optimize, and maintain
           responsive websites using HTML, CSS, Javascript, PHP, &amp; WordPress.
