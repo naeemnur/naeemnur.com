d9175cd25d3cf6659c49cb04508dd079f4acfa25
naeem <naeem@paytabs.com>
2019-09-02 10:07:09 +0300

    fixed react-snap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { hydrate, render } from "react-dom";
 import App from "./App";
 import "./scss/style.scss";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// react-snap magic
+const rootElement = document.getElementById("root");
+
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
