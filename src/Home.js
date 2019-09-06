711d76e350e357c06c1a43f65a433c843c769826
naeemMac <naeemnur@gmail.com>
2019-09-06 19:58:39 +0300

    made projects a proper reusable component

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,9 @@
 import React, { Component, Fragment } from "react";
 import { Helmet } from "react-helmet";
 import { NavLink } from "react-router-dom";
+import { Project } from "./Projects";
 
 class Home extends Component {
-
   render() {
     const pageTitle = "Hi, I'm Naeem.";
     const pageContent = `I’m a self-taught Web / Frontend developer. I mostly work around WordPress, PHP, MySQL. I'm currently focusing on JavaScript, React.js and JAMStack. I'm a creator at heart, so I enjoy taking a raw thought and building it to completion.`;
@@ -16,50 +16,30 @@ class Home extends Component {
           <meta name="description" content="{pageContent }" />
         </Helmet>
         <div className="nn_hero">
-            <h1>{pageTitle}</h1>
-            <p>{pageContent}</p>
+          <h1>{pageTitle}</h1>
+          <p>{pageContent}</p>
         </div>
 
         <div className="nn_featured">
-          <h2>
-            {projectHeading}
-          </h2>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            className="nn_project nn_gcemetery"
-            href="https://gcemetery.co"
-          >
-            <h4 className="nn_project-title">The Google Cemetery</h4>
-            <p className="nn_project-desc">
-              Complete list of dead Google products / services, when and why
-              they died.
-            </p>
-            <div className="nn_project-meta">
-              <span className="nn_project-url">gcemetery.co</span>
-              <span title="Date launched" className="nn_project-date">
-                November 2018
-              </span>
-            </div>
-          </a>
+          <h2>{projectHeading}</h2>
+          <Project
+            url="https://gcemetery.co/"
+            title="The Google Cemetery"
+            desc="Complete list of dead Google products / services, when and why
+            they died."
+            domain="gcmetery.co"
+            date="November 2018"
+            class="nn_project nn_gcemetery"
+          />
 
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            className="nn_project nn_acquiredby"
-            href="https://acquiredby.co"
-          >
-            <h4 className="nn_project-title">AcquiredBy</h4>
-            <p className="nn_project-desc">
-              AcquiredBy is a growing database of tech industry acquisitions.
-            </p>
-            <div className="nn_project-meta">
-              <span className="nn_project-url">acquiredby.co</span>
-              <span title="Date launched" className="nn_project-date">
-                April 2017
-              </span>
-            </div>
-          </a>
+          <Project
+            url="https://acquiredby.co/"
+            title="AcquiredBy"
+            desc="AcquiredBy is a growing database of tech industry acquisitions."
+            domain="acquiredby.co"
+            date="June 2017"
+            class="nn_project nn_acquiredby"
+          />
 
           <NavLink className="nn_view-projects" to="/projects/" strict>
             View all projects &rarr;
--- src/Projects.js
+++ src/Projects.js
@@ -1,5 +1,35 @@
 import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
 import { Helmet } from "react-helmet";
+
+export const Project = props => {
+  return (
+    <a
+      target="_blank"
+      rel="noopener noreferrer"
+      className={props.class}
+      href={props.url}
+    >
+      <h4 className="nn_project-title">{props.title}</h4>
+      <p className="nn_project-desc">{props.desc}</p>
+      <div className="nn_project-meta">
+        <span className="nn_project-url">{props.domain}</span>
+        <span title="Date launched" className="nn_project-date">
+          {props.date}
+        </span>
+      </div>
+    </a>
+  );
+};
+
+Project.propTypes = {
+  title: PropTypes.string,
+  desc: PropTypes.string,
+  domain: PropTypes.string,
+  date: PropTypes.string,
+  url: PropTypes.string,
+  class: PropTypes.string,
+};
 
 class Projects extends Component {
   render() {
@@ -13,114 +43,60 @@ class Projects extends Component {
         </Helmet>
         <h1 className="nn_page-title">{pageTitle}</h1>
         <div className="nn_featured">
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            className="nn_project nn_gcemetery"
-            href="https://gcemetery.co"
-          >
-            <h4 className="nn_project-title">The Google Cemetery</h4>
-            <p className="nn_project-desc">
-              Complete list of dead Google products / services, when and why
-              they died.
-            </p>
-            <div className="nn_project-meta">
-              <span className="nn_project-url">gcemetery.co</span>
-              <span title="Date launched" className="nn_project-date">
-                November 2018
-              </span>
-            </div>
-          </a>
+          <Project
+            url="https://gcemetery.co/"
+            title="The Google Cemetery"
+            desc="Complete list of dead Google products / services, when and why
+            they died."
+            domain="gcmetery.co"
+            date="November 2018"
+            class="nn_project nn_gcemetery"
+          />
 
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            className="nn_project nn_acquiredby"
-            href="https://acquiredby.co"
-          >
-            <h4 className="nn_project-title">AcquiredBy</h4>
-            <p className="nn_project-desc">
-              AcquiredBy is a growing database of tech industry acquisitions.
-            </p>
-            <div className="nn_project-meta">
-              <span className="nn_project-url">acquiredby.co</span>
-              <span title="Date launched" className="nn_project-date">
-                April 2017
-              </span>
-            </div>
-          </a>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            className="nn_project nn_exchainged"
-            href="https://exchainged.co"
-          >
-            <h4 className="nn_project-title">ExChainged</h4>
-            <p className="nn_project-desc">
-              A hub for token rates, tokens, and exchanges that support them.
-            </p>
-            <div className="nn_project-meta">
-              <span className="nn_project-url">exchainged.co</span>
-              <span title="Date launched" className="nn_project-date">
-                February 2019
-              </span>
-            </div>
-          </a>
+          <Project
+            url="https://acquiredby.co/"
+            title="AcquiredBy"
+            desc="AcquiredBy is a growing database of tech industry acquisitions"
+            domain="acquiredby.co"
+            date="June 2017"
+            class="nn_project nn_acquiredby"
+          />
 
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            className="nn_project nn_faang"
-            href="https://faangwat.ch"
-          >
-            <h4 className="nn_project-title">FAANGWatch</h4>
-            <p className="nn_project-desc">
-              End of day stock prices of key tech giants
-            </p>
-            <div className="nn_project-meta">
-              <span className="nn_project-url">faangwat.ch</span>
-              <span title="Date launched" className="nn_project-date">
-                April 2019
-              </span>
-            </div>
-          </a>
+          <Project
+            url="https://exchainged.co/"
+            title="ExChainged"
+            desc="A hub for token rates, tokens, and exchanges that support them"
+            domain="exchainged.co"
+            date="February 2019"
+            class="nn_project nn_exchainged"
+          />
 
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            className="nn_project nn_urepublic"
-            href="https://unicornrepublic.co"
-          >
-            <h4 className="nn_project-title">Unicorn Republic</h4>
-            <p className="nn_project-desc">
-              Curated list of startups that evolved into $1 Billion+
-              corporations
-            </p>
-            <div className="nn_project-meta">
-              <span className="nn_project-url">unicornrepublic.co</span>
-              <span title="Date launched" className="nn_project-date">
-                Feb 2019
-              </span>
-            </div>
-          </a>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            className="nn_project nn_workingtime"
-            href="https://workingti.me"
-          >
-            <h4 className="nn_project-title">Working Time</h4>
-            <p className="nn_project-desc">
-              Working Time displays the average working hours per week around
-              the globe
-            </p>
-            <div className="nn_project-meta">
-              <span className="nn_project-url">workingti.me</span>
-              <span title="Date launched" className="nn_project-date">
-                Feb 2019
-              </span>
-            </div>
-          </a>
+          <Project
+            url="https://faangwat.ch/"
+            title="FAANGWatch"
+            desc="End of day stock prices of key tech giants"
+            domain="faangwat.ch"
+            date="March 2019"
+            class="nn_project nn_faang"
+          />
+
+          <Project
+            url="https://unicornrepublic.co/"
+            title="Unicorn Republic"
+            desc="Curated list of startups that evolved into $1 Billion+ corporations"
+            domain="unicornrepublic.co"
+            date="February 2019"
+            class="nn_project nn_urepublic"
+          />
+
+          <Project
+            url="https://workingti.me/"
+            title="Working Time"
+            desc="Working Time displays the average working hours per week around the globe"
+            domain="workingti.me"
+            date="January 2019"
+            class="nn_project nn_workingtime"
+          />
         </div>
       </Fragment>
     );
